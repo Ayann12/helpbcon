3aafd5cd35ce53d4de587bdf6f0c62f1ddd1002d
Ayann12 <tantowi818@gmail.com>
2026-01-29 13:55:49 +0700

    edit footer

--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -226,81 +226,11 @@
 		<footer class="footer">
 			<div class="container">
 				<div class="widget-wrapper">
-					<div class="row">
-						<div class="col-xl-3 col-md-6">
-							<div class="footer-widget">
-								<div class="logo mb-35">
-									<a href="index.html"> <img src="<?= base_url('assets/homepage/'); ?>assets/img/logo/logo.svg" alt=""> </a>
-								</div>
-								<p class="desc mb-35">We are expert designer team, There have a lot of designer and developer If you have any project you can hire Create a website.</p>
-								<ul class="socials">
-									<li>
-										<a href="jvascript:void(0)"> <i class="lni lni-facebook-filled"></i> </a>
-									</li>
-									<li>
-										<a href="jvascript:void(0)"> <i class="lni lni-twitter-filled"></i> </a>
-									</li>
-									<li>
-										<a href="jvascript:void(0)"> <i class="lni lni-instagram-filled"></i> </a>
-									</li>
-									<li>
-										<a href="jvascript:void(0)"> <i class="lni lni-linkedin-original"></i> </a>
-									</li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-xl-2 offset-xl-1 col-md-5 offset-md-1 col-sm-6">
-							<div class="footer-widget">
-								<h3>Link</h3>
-								<ul class="links">
-									<li> <a href="javascript:void(0)">Home</a> </li>
-									<li> <a href="javascript:void(0)">About</a> </li>
-									<li> <a href="javascript:void(0)">Services</a> </li>
-									<li> <a href="javascript:void(0)">Portfolio</a> </li>
-									<li> <a href="javascript:void(0)">Pricing</a> </li>
-									<li> <a href="javascript:void(0)">Team</a> </li>
-									<li> <a href="javascript:void(0)">Contact</a> </li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-xl-3 col-md-6 col-sm-6">
-							<div class="footer-widget">
-								<h3>Services</h3>
-								<ul class="links">
-									<li> <a href="javascript:void(0)">Graphic design</a> </li>
-									<li> <a href="javascript:void(0)">Web design</a> </li>
-									<li> <a href="javascript:void(0)">Visual design</a> </li>
-									<li> <a href="javascript:void(0)">Product design</a> </li>
-									<li> <a href="javascript:void(0)">UI/UX design</a> </li>
-									<li> <a href="javascript:void(0)">Web development</a> </li>
-									<li> <a href="javascript:void(0)">Startup business</a> </li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-xl-3 col-md-6">
-							<div class="footer-widget">
-								<h3>Contact</h3>
-								<ul>
-									<li>+003894372632</li>
-									<li>helldesigner@gmail.ccom</li>
-									<li>United state of America</li>
-								</ul>
-								<div class="contact_map" style="width: 100%; height: 150px; margin-top: 25px;">
-									<div class="gmap_canvas">
-										<iframe id="gmap_canvas" src="https://maps.google.com/maps?q=Mission%20District%2C%20San%20Francisco%2C%20CA%2C%20USA&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" style="width: 100%;"></iframe>
-									</div>
-									</div>
-							</div>
-						</div>
-
-					</div>
+					
 				</div>
 
 				<div class="copy-right">
-					<p>Design and Developed by <a href="https://uideck.com" rel="nofollow" target="_blank"> UIdeck </a>. Distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a></p>
+					<p>© 2026 IT Bcon. All Rights Reserved.</p>
 				</div>
 
 			</div>
